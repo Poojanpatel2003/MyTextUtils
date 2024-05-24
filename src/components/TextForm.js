4d1c5f74aea3e4a17a32f040ed7817846a4add9c
Poojanpatel2003 <144653133+Poojanpatel2003@users.noreply.github.com>
2024-05-24 15:02:30 +0530

    Update TextForm.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,13 +6,25 @@ export default function TextForm(props) {
         let newText=text.toUpperCase();
         settext(newText);
     }
+    const handledownClick=()=>{
+        let newText=text.toLowerCase();
+        settext(newText);
+    }
     const handleOnChange=(event)=>{
         settext(event.target.value)
+    }
+    const handleclearClick=()=>{
+        let newText='';
+        settext(newText);
     }
     const handlecopy=(event)=>{
         var text=document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraSpaces=()=>{
+        let newText=text.split(/[ ]+/);
+        settext(newText.join(" "))
     }
     const [text, settext]= useState('#');
   return (
@@ -24,13 +36,16 @@ export default function TextForm(props) {
         <h5>Example Textarea</h5>
     </label>
 
-     <textarea className="form-control"value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'grey':'white',color:props.mode==='dark'?'white':'black'}}id="myBox" cols="100" rows="8"/> 
+     <textarea className="form-control"value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'fffcc':'white',color:props.mode==='dark'?'black':'black'}}id="myBox" cols="100" rows="8"/> 
     
      </div>
      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+     <button className="btn btn-primary mx-3" onClick={handledownClick}>Convert to Lowercase</button>
+     <button className="btn btn-primary mx-3" onClick={handleclearClick}>Clear Text</button>
   
     
      <button className="btn btn-primary mx-2" onClick={handlecopy} >Copy Text</button>
+     <button className="btn btn-primary mx-2" onClick={handleExtraSpaces} >Remove Extraspace</button>
     </div>
 
     <div className="container my-2" style={{color:props.mode==='dark'?'white':'black'}}>
